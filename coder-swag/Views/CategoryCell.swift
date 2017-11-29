0c38bd210a929c7c0f1d9c75708ad6bd1ebf2209
Donald Belliveau <donsb@icloud.com>
2017-11-29 11:39:45 -0800

    Created Cocoa Touch file in Views called CategoryCell.swift for control of TableViewCells.

---
+++ coder-swag/Views/CategoryCell.swift
@@ -0,0 +1,24 @@
+//
+//  CategoryCell.swift
+//  coder-swag
+//
+//  Created by Donald Belliveau on 2017-11-29.
+//  Copyright © 2017 Donald Belliveau. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
