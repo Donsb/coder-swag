5d7118f320a86b0c9493fade1b0682cb0bffc1bf
Donald Belliveau <donsb@icloud.com>
2017-11-29 11:12:37 -0800

    Initial Commit.  Renamed ViewControllerVC to CategoriesVC.

--- coder-swag/CategoriesVC.swift
+++ coder-swag/CategoriesVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CategoriesVC.swift
 //  coder-swag
 //
 //  Created by Donald Belliveau on 2017-11-29.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CategoriesVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
