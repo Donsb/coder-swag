53f52de2518c1397623366cfe1fe0390bf19ce6f
Donald Belliveau <donsb@icloud.com>
2017-11-29 13:05:04 -0800

    created struct for Category with variables and initializer.

--- coder-swag/Model/Category.swift
+++ coder-swag/Model/Category.swift
@@ -7,3 +7,15 @@
 //
 
 import Foundation
+
+struct Category {
+    // Public Variables that can be fetched but not Set!
+    private(set) public var title:String
+    private(set) public var imageName: String
+    
+    // Initializer that sets the title and imageName on creation.
+    init(title: String, imageName: String) {
+        self.title = title
+        self.imageName = imageName
+    }
+}
