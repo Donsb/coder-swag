960bbfb13f85628ab7e857caee9990c3247b114e
Donald Belliveau <donsb@icloud.com>
2017-11-29 11:56:42 -0800

    Added IBOutlet for TableView in CategoriesVC.  Connected the Outlet to UIObject.

--- coder-swag/Controller/CategoriesVC.swift
+++ coder-swag/Controller/CategoriesVC.swift
@@ -9,17 +9,42 @@
 import UIKit
 
 class CategoriesVC: UIViewController {
-
+    
+    /*
+     IBOutlets
+     */
+    @IBOutlet weak var categoryTable: UITableView!
+    
+    /*
+     Functions
+     */
+    
+    
+    /*
+     View Did Load Function.
+     */
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    /*
+     END View Did Load.
+     */
+    
+    
+    /*
+     Did Receive Memory Warning Function.
+     */
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    /*
+     END Did Receive Memory Warning Function.
+     */
+    
+    
 }
 
--- coder-swag/Views/CategoryCell.swift
+++ coder-swag/Views/CategoryCell.swift
@@ -17,15 +17,33 @@ class CategoryCell: UITableViewCell {
     @IBOutlet weak var categoryImage: UIImageView!
     @IBOutlet weak var categoryTitle: UILabel!
     
+    /*
+     Functions
+     */
+    
+    /*
+     Awake From Nib Function.
+     */
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    /*
+     END Awake From Ninb Function.
+     */
+    
+    
+    /*
+     Set Selected Function
+     */
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
     }
-
+    /*
+     END Set Selection Function.
+     */
 }
