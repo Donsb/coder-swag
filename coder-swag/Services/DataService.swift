ee0519e6272951031d827523a737ea7ba5f35f1b
Donald Belliveau <donsb@icloud.com>
2017-11-29 13:18:21 -0800

    Created a static constant of Dataservice.  Created an Array of Category class inits to represent a web server.  Create a function to retreive the data.

--- coder-swag/Services/DataService.swift
+++ coder-swag/Services/DataService.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+
+class DataService {
+    /*
+     Static means there is only ever 1 copy of it in memory.
+     In other words, there is only 1 copy of our data!
+     */
+    static let instance = DataService()
+    
+    // This is to simulate the Server we would contact.  Or for a small business, the
+    // Data they have.
+    private let categories = [
+        Category(title: "SHIRTS", imageName: " shirts.png"),
+        Category(title: "HOODIES", imageName: "hoodies.png"),
+        Category(title: "HATS", imageName: "hats.png"),
+        Category(title: "DIGITAL", imageName: "digital.png")
+    ]
+    
+    // Function that would contact a Server and retreive Data.
+    // Just a simulation as no Server actually exists.
+    func getCategories()-> [Category] {
+        return categories
+    }
+    
+    
+}
