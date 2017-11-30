9f83331a0b3a06cc2ad8c4afab9b05dfc9dde387
Donald Belliveau <donsb@icloud.com>
2017-11-30 09:29:38 -0800

    Created the Data-> Arrays of Product for shirts, hats, hoosies and a blank array for Digital.

--- coder-swag/Services/DataService.swift
+++ coder-swag/Services/DataService.swift
@@ -24,6 +24,35 @@ class DataService {
         Category(title: "DIGITAL", imageName: "digital.png")
     ]
     
+    // Product Data for Hats Category.
+    private let hats = [
+        Product(title: "Devslopes Logo Graphic Beanie", price: "$18", imageName: "hat01.png"),
+        Product(title: "Devslopes Logo Hat Black", price: "$22", imageName: "hat02.png"),
+        Product(title: "Devslopes Logo Hat White", price: "$22", imageName: "hat03.png"),
+        Product(title: "Devslopes Logo Snapback", price: "$20", imageName: "hat04.png")
+    ]
+    
+    // Product Data for Hoodies.
+    private let hoodies = [
+        Product(title: "Devslopes Logo Hoodie Grey", price: "$32", imageName: "hoodie01.png"),
+        Product(title: "Devslopes Logo Hoodie Red", price: "$32", imageName: "hoodie02.png"),
+        Product(title: "Devslopes Hoodie Grey", price: "$32", imageName: "hoodie03.png"),
+        Product(title: "Devslopes Hoodie Black", price: "$32", imageName: "hoodie04.png")
+    ]
+    
+    // Product Data for Shirts.
+    private let shirts = [
+        Product(title: "Devslopes Logo Shirt Black", price: "$18", imageName: "shirt01.png"),
+        Product(title: "Devslopes Badge Shirt Light Grey", price: "$19", imageName: "shirt02.png"),
+        Product(title: "Devslopes Logo Shirt Red", price: "$18", imageName: "shirt03.png"),
+        Product(title: "Hustle Delegate Grey", price: "$18", imageName: "shirt04.png"),
+        Product(title: "Kickflip Studios Black", price: "$18", imageName: "shirt05.png")
+    ]
+    
+    // Product Data for Digital.  Empty Array of Type Product.
+    private let digitalGoods = [Product]()
+    
+    
     // Function that would contact a Server and retreive Data.
     // Just a simulation as no Server actually exists.
     func getCategories()-> [Category] {
@@ -32,3 +61,14 @@ class DataService {
     
     
 }
+
+
+
+
+
+
+
+
+
+
+
