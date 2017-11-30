dde851fce9e21ea354690ea56ce417abb211e9f2
Donald Belliveau <donsb@icloud.com>
2017-11-29 20:02:45 -0800

    Added new ViewController to Storyboard and connected to segue.  Added Collection View to new VC.  Added imageView and labels to to cell.  New Cocoa Touch CollectionViewCell file names ProductCell to View group folder.

---
+++ coder-swag/Views/ProductViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ProductViewCell.swift
+//  coder-swag
+//
+//  Created by Donald Belliveau on 2017-11-29.
+//  Copyright © 2017 Donald Belliveau. All rights reserved.
+//
+
+import UIKit
+
+class ProductViewCell: UICollectionViewCell {
+    
+}
+
